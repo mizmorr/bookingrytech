bc548d6fc8cdda00a63fc1da5c69b8a37d14d6ef
mizmorr <mizmorrr@gmail.com>
2025-03-11 11:24:57 +0300

    feat: create service layer, (service rename from book)

---
+++ internal/service/methods.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/mizmorr/ingrytech/internal/domain"
+	"github.com/mizmorr/ingrytech/internal/mappers"
+)
+
+func (svc *BookService) Get(ctx context.Context, id uuid.UUID) (*domain.Book, error) {
+	bookDB, err := svc.repo.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return mappers.BookToWeb(bookDB), nil
+}
+
+func (svc *BookService) Create(ctx context.Context, book *domain.Book) {
+	bookDB := mappers.BookToDB(book)
+
+	svc.repo.Create(ctx, bookDB)
+}
+
+func (svc *BookService) Update(ctx context.Context, book *domain.Book) error {
+	bookDB := mappers.BookToDB(book)
+
+	return svc.repo.Update(ctx, bookDB)
+}
+
+func (svc *BookService) GetAll(ctx context.Context) ([]*domain.Book, error) {
+	books, err := svc.repo.GetAll(ctx)
+	if err != nil {
+		return []*domain.Book{}, err
+	}
+	booksToWeb := mappers.BooksToWeb(books)
+
+	return booksToWeb, err
+}
---
+++ internal/service/service.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/mizmorr/ingrytech/internal/store/model"
+)
+
+type Repository interface {
+	GetAll(ctx context.Context) ([]*model.Book, error)
+	Get(ctx context.Context, id uuid.UUID) (*model.Book, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+	Update(ctx context.Context, newBookData *model.Book) error
+	Create(ctx context.Context, book *model.Book)
+}
+
+type BookService struct {
+	repo Repository
+}
