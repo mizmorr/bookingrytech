15191298781cd836b54b6a5ae025624b8c1e4d3e
mizmorr <mizmorrr@gmail.com>
2025-03-12 17:19:34 +0300

    feat: add swagger declarations

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,24 @@ import (
 	"os/signal"
 	"syscall"
 
+	_ "github.com/mizmorr/ingrytech/docs"
 	"github.com/mizmorr/ingrytech/internal/app"
 )
 
+// @title           Swagger API
+// @version         1.0
+// @description     This is a book aggregation service.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
 func main() {
 	if err := execute(); err != nil {
 		panic(err)
