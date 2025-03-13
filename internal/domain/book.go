6b7cb19dc1498a1d1ec1aa005ae7142ff048f8d7
mizmorr <mizmorrr@gmail.com>
2025-03-13 12:36:35 +0300

    fix: change tags

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -3,8 +3,8 @@ package domain
 import "github.com/google/uuid"
 
 type Book struct {
-	ID              uuid.UUID `json:"id"`
-	Title           string    `json:"title,omitempty"`
-	Author          string    `json:"author,omitempty"`
-	PublicationYear uint16    `json:"publication_year,omitempty"`
+	ID              uuid.UUID `json:"id" validate:"required"`
+	Title           string    `json:"title"`
+	Author          string    `json:"author"`
+	PublicationYear uint16    `json:"publication_year"`
 }
